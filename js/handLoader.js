99c424b09d090dfa61f8056c8a38c5571869a78e
nathanidecker <nathanidecker@users.noreply.github.com>
2019-01-20 01:42:50 -0800

    Update handLoader.js

--- js/handLoader.js
+++ js/handLoader.js
@@ -37,9 +37,9 @@ function init() {
 
   var material = new THREE.MeshPhongMaterial( { color: 0x0e2045, specular: 0x111111, shininess: 200 } );
   
-  var hash = window.location.hash;
-  
-  loader.load(hash, function ( geometry ) {
+var type = window.location.hash.substr(1);
+
+  loader.load(type, function ( geometry ) {
     var mesh = new THREE.Mesh( geometry, material );
 
     mesh.position.set( 0, 0, 0 );
