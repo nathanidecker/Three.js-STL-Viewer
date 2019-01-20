becd0a51a03cd4d35ffa5d31552fa22f13d090ca
nathanidecker <nathanidecker@users.noreply.github.com>
2019-01-20 01:35:40 -0800

    Update handLoader.js

--- js/handLoader.js
+++ js/handLoader.js
@@ -36,7 +36,10 @@ function init() {
   // Binary files
 
   var material = new THREE.MeshPhongMaterial( { color: 0x0e2045, specular: 0x111111, shininess: 200 } );
-  loader.load( 'https://s3.amazonaws.com/limbforgestls/EbeArm/Ebe_forearm_L/forearm_L_C4-200_L1-230.stl', function ( geometry ) {
+  
+  var hash = window.location.hash;
+  
+  loader.load(hash, function ( geometry ) {
     var mesh = new THREE.Mesh( geometry, material );
 
     mesh.position.set( 0, 0, 0 );
